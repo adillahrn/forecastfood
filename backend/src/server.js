ba5dff8d6ada60bae8477cae334a5ecd2509e205
frabencyy <nailahadiantihermawan@gmail.com>
2026-06-03 10:34:34 +0700

    fix port

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-  console.log(`📡 Health check: http://localhost:${PORT}/api/health`);
+  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`📡 Health check: /api/health`);
 });
